91900077fec50540e14e8cc7c608ecb95a721a43
Parham <sanatiparham7@gmail.com>
2025-06-22 15:34:19 +0330

    script CreateTask Add shod

--- vue-project/src/components/CreateTask.vue
+++ vue-project/src/components/CreateTask.vue
@@ -1,18 +1,29 @@
 <script setup >
+import { ref } from 'vue'
+
+const newTask = ref('')
+const newDate = ref('')
+const Description = ref('')
+
+function createTask() {
+  const tasks = JSON.parse(localStorage.getItem('tasks')) || [] ;
+  tasks.push({title: newTask.value, newDate: newDate.value , Description: Description.value , done: false })
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
 </script>
 
 <template>
   <div class="create-container">
     <h1>Create New Task</h1>
-    <form>
+    <form @submit.prevent="createTask">
       <label >Title</label>
-      <input type="text"  placeholder="Enter Title"  required>
+      <input type="text" v-model="newTask" placeholder="Enter Title"  required>
 
       <label >Date</label>
-      <input type="date" placeholder="Enter Date" required>
+      <input type="date" v-model="newDate" placeholder="Enter Date" required>
 
       <label >Description</label>
-      <textarea placeholder="Enter Description" required></textarea>
+      <textarea v-model="Description" placeholder="Enter Description" required></textarea>
 
       <button type="submit">Add Task</button>
     </form>
