52467066238fb4f393f0d114888ec01963d22334
Parham <sanatiparham7@gmail.com>
2025-06-22 15:10:13 +0330

    template CreateTask add shod

---
+++ vue-project/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <router-view />
+
+</template>
+
+<script setup>
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  font-family: Arial, sans-serif;
+}
+</style>
---
+++ vue-project/src/components/CreateTask.vue
@@ -0,0 +1,23 @@
+<script setup >
+</script>
+
+<template>
+  <div class="create-container">
+    <h1>Create New Task</h1>
+    <form>
+      <label >Title</label>
+      <input type="text"  placeholder="Enter Title"  required>
+
+      <label >Date</label>
+      <input type="date" placeholder="Enter Date" required>
+
+      <label >Description</label>
+      <textarea placeholder="Enter Description" required></textarea>
+
+      <button type="submit">Add Task</button>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+</style>
